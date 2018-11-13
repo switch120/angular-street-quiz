6ef037fc6cd180f65e85aa474d41fff23e9f91e8
switch120 <switch120@gmail.com>
2018-11-12 21:01:06 -0500

    cloud function cleanup

--- angular-street-quiz/functions/index.js
+++ angular-street-quiz/functions/index.js
@@ -1,12 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-// admin.initializeApp(functions.config().firebase);
-
-// let cert = require("./accountKey.json");
-
-// console.log(cert);
-
 admin.initializeApp();
 
 let express = require('express');
@@ -18,17 +12,8 @@ const app = express();
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
-// zipCodeApi route
 app.use('/mapdata', async (req, res) => {
 
-    // admin.storage().bucket().getFiles().then(files => {
-    //     let _files = files[0];
-    //     _files.shift();
-    //     res.json(_files.map(file => {
-    //         file.url = await getDownloadUrl(file);
-    //         return file;
-    //     }));
-    // });
     const parseFiles = async function () {
 
         try {
@@ -38,20 +23,6 @@ app.use('/mapdata', async (req, res) => {
             files.shift();
 
             return files;
-
-            // return files.map( async file => {
-            //     try {
-            //         // const fileRef = admin.storage().ref();
-            //         file.url = await file.getSignedUrl({
-            //             action: "read",
-            //             expires: `01-01-2192`
-            //         })
-            //     }
-            //     catch (ex) { console.log(ex); }
-                
-            //     console.log(file);
-            //     return file;
-            // });
         }
         catch (exc){
             console.log(exc);
@@ -62,25 +33,11 @@ app.use('/mapdata', async (req, res) => {
 
     try {
         res.json(await parseFiles());
-        // console.log(parseFiles());
-        // Promise.all(parseFiles()).then(data => res.json(data));
     }
     catch (exc){
         console.log(exc);
     }
-
-    // res.json(parseFiles());
 });
-
-// async function loadBucket(){
-//     return await admin.storage().bucket().getFiles();
-// }
-
-// async function getDownloadUrl(file) {
-//      return await file.getDownloadUrl();
-// }
 
 // pass the express app to the HTTPS Trigger
 exports.api = functions.https.onRequest(app);
-
-// app.listen(80);
